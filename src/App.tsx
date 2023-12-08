a2ed902674fa350e089cb6babceea6274a608608
Serg Nakovnick <w8smile@gmail.com>
2023-12-08 15:37:06 +0300

    button

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    // const myFirstSubscriber = () => {
+    //     console.log("I am Vasya!")
+    // }
+    //
+    // const secondSubscriber = () => {
+    //     console.log("I am Petya!")
+    // }
+
+
+    const onClickHandler = (name:string) => {
+        console.log(name)
+    }
+// const foo2 =(num:number) => {
+//     console.log(num)
+//
+// }
+//
+//     const foo1 = ()=> {
+//         console.log(100200)
+//     }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={(event)=>{console.log("Ivan!")}}>MyYouTubeChanel-1</button>
+      <button onClick={(event)=>onClickHandler("Vasya")}>MyYouTubeChanel-2</button>
+      {/*  <button onClick={foo1}>1</button>*/}
+      {/*  <button onClick={()=>foo2(10200)}>2</button>*/}
     </div>
   );
 }
